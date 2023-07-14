aa68b7af22960c16c4ba6f9e3badd66352317244
Sandeep Kalyan Sunkara <44454835+SandeepKalyan@users.noreply.github.com>
2023-07-14 19:01:16 -0400

    Established Request-Response Initiation

--- requestHandler/__init__.py
+++ requestHandler/__init__.py
@@ -1,10 +1,24 @@
+import json
 from http.server import BaseHTTPRequestHandler
 
 
 class RequestHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
-        print(self.path)
+        # Set the response status code
+        self.send_response(200)
+
+        # Set the response headers
+        self.send_header('Content-type', 'Initiation')
+
+        self.end_headers()
+
+        # Set the response content
+        response = 'Hello'
+        self.wfile.write(response.encode())
 
     def do_POST(self):
         print(self.path)
+        self.send_header("header:", "Version-1")
+        self.send_response(200, "Response body")
+        self.end_headers()
